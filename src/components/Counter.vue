e358e6f817e412b429f6cff1ccf9018d6ed9c68f
lightzane <jp.eaguilar@gmail.com>
2025-06-14 17:06:41 +0800

    5 - learn emit using defineEmits with typescript interface

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,10 +2,11 @@
 import { onMounted, ref } from 'vue';
 
 import fetchCount from '@/services/fetchCount';
+import ControlBar from '@/components/ControlBar.vue';
 
 type Props = {
-    limit: number;
-    alertMessageOnLimit?: string; // if optional, then add `withDefaults()` on defineProps
+  limit: number;
+  alertMessageOnLimit?: string; // if optional, then add `withDefaults()` on defineProps
 };
 
 // Compiler macro
@@ -14,36 +15,38 @@ type Props = {
 // = no need to be imported.
 // = can ONLY BE USED inside a "script setup" block
 const props = withDefaults(defineProps<Props>(), {
-    alertMessageOnLimit: 'Max limit is reached.'
-})
+  alertMessageOnLimit: 'Max limit is reached.',
+});
 
 const count = ref<number | null>(null);
 
 onMounted(() => {
-    fetchCount((initialCount) => {
-        count.value = initialCount;
-    });
+  fetchCount((initialCount) => {
+    count.value = initialCount;
+  });
 });
 
 function addCount() {
-    if (count.value === null) {
-        return;
-    }
+  if (count.value === null) {
+    return;
+  }
 
-    if (count.value >= props.limit) {
-        alert(props.alertMessageOnLimit);
-    } else {
-        count.value++;
-    }
-
+  if (count.value >= props.limit) {
+    alert(props.alertMessageOnLimit);
+  } else {
+    count.value++;
+  }
 }
 </script>
 
 <template>
-    <div>
-        <p>{{ count }}</p>
-        <p>
-            <button @click="addCount">Add</button>
-        </p>
-    </div>
+  <div>
+    <p>{{ count }}</p>
+
+    <!-- prettier-ignore -->
+    <ControlBar 
+      @add-count="addCount"
+      @reset-count="count = 0"
+    ></ControlBar>
+  </div>
 </template>
