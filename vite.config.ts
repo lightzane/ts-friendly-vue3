a49a8e5730f19fc2cfed3e11e68d0140ad800b62
lightzane <jp.eaguilar@gmail.com>
2025-06-14 17:16:02 +0800

    update public base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ts-friendly-vue3', // the repo name if deploying to github pages
   plugins: [
     vue(),
     vueDevTools(),
